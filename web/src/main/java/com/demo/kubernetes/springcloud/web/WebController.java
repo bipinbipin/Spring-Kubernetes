eb950f8fb360cb3c302598bceba3b9ec67dfe2b8
bipin <bipinbipin@gmail.com>
2019-01-24 16:25:44 -0600

    null check acceptable city names

--- web/src/main/java/com/demo/kubernetes/springcloud/web/WebController.java
+++ web/src/main/java/com/demo/kubernetes/springcloud/web/WebController.java
@@ -30,10 +30,12 @@ public class WebController {
 		Gson gson = new Gson();
 		String response = service.getZipInfo(zipcode);
 		logger.info(response);
-		ZipCodeInfo info = gson.fromJson(response,ZipCodeInfo.class);
+		ZipCode info = gson.fromJson(response, ZipCode.class);
 
 		logger.info("============");
-		logger.info(info.zip_code);
+		logger.info(info.places[0].place_name);
+		logger.info("============");
+
 		StringBuilder result = new StringBuilder();
 		result.append("<html><body>");
 		if(info != null){
@@ -43,78 +45,6 @@ public class WebController {
 		return result.toString();
 	}
 	
-	@RequestMapping(value = "/zip/getNearbyZipcodes/{zipcode}/{distance}", produces = { "text/html" })
-	public String zipDistance(@PathVariable("zipcode") String zipcode,@PathVariable("distance") String distance) {
-		Gson gson = new Gson();
-		String response = service.getNearbyZipcodesWithinDistance(zipcode, distance);
-		logger.info(response);
-		ZipCodeWrapper zipCodes = gson.fromJson(response,ZipCodeWrapper.class);
-		StringBuilder result = new StringBuilder();
-		result.append("<html><body>");
-		if(zipCodes != null){
-			result.append(zipCodes.toString());
-		}
-		result.append("</body></html>");
-		return result.toString();
-	}
-}
-
-class ZipCodeWrapper{
-	ZipCode[] zip_codes;
-
-	public ZipCode[] getZip_codes() {
-		return zip_codes;
-	}
-
-	public void setZip_codes(ZipCode[] zip_codes) {
-		this.zip_codes = zip_codes;
-	}
-
-	public String toString() {
-		StringBuilder strBldr = new StringBuilder();
-		strBldr.append("<p>Zip codes:<br>"); 
-		for(ZipCode zipCode:zip_codes){
-			strBldr.append(zipCode);
-		}
-		return strBldr.toString();
-	}
-}
-
-class ZipCode {
-    String zip_code;
-	String distance;
-	String city;
-	String state;
-	public String getZip_code() {
-		return zip_code;
-	}
-	public void setZip_code(String zip_code) {
-		this.zip_code = zip_code;
-	}
-	public String getDistance() {
-		return distance;
-	}
-	public void setDistance(String distance) {
-		this.distance = distance;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	public String toString() {
-		return "<p>zip_code=" + zip_code + ", distance=" + distance + " miles, city=" + city + ", state=" + state;
-	}
-}
-
 /*
 {
 	"post code": "33301",
@@ -133,96 +63,80 @@ class ZipCode {
  */
 
 
-
-class ZipCodeInfo {
-    @SerializedName("post code") String zip_code;
+class ZipCode {
+	@SerializedName("post code") String post_code;
+	@SerializedName("country abbreviation") String country_abbr;
 	String country;
-    @SerializedName("country abbreviation") String country_abbr;
-//	String city;
-//	String state;
-//	CityState[] places;
-//	Timezone timezone;
+	Place[] places;
 
+	public String getPost_code() {	return post_code; }
+	public void setPost_code(String post_code) { this.post_code = post_code; }
+	public String getCountry_abbr() { return country_abbr; }
+	public void setCountry_abbr(String country_abbr) { this.country_abbr = country_abbr; }
+	public String getCountry() { return country; }
+	public void setCountry(String country) { this.country = country; }
+	public Place[] getPlaces() {return places; }
+	public void setPlaces(Place[] places) {	this.places = places; }
 
-    public String getZip_code() {
-        return zip_code;
-    }
-
-    public void setZip_code(String zip_code) {
-        this.zip_code = zip_code;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCountry_abbr() {
-        return country_abbr;
-    }
-
-    public void setCountry_abbr(String country_abbr) {
-        this.country_abbr = country_abbr;
-    }
-
-    class Timezone{
-		String timezone_identifier;
-		String timezone_abbr;
-		public String getTimezone_identifier() {
-			return timezone_identifier;
-		}
-		public void setTimezone_identifier(String timezone_identifier) {
-			this.timezone_identifier = timezone_identifier;
-		}
-		public String getTimezone_abbr() {
-			return timezone_abbr;
-		}
-		public void setTimezone_abbr(String timezone_abbr) {
-			this.timezone_abbr = timezone_abbr;
-		}
-		
-		public String toString() {
-			return "<p>Timezone: " + timezone_identifier + " (" + timezone_abbr + ")";
-		}
-	}
-	
-	class CityState{
-		String city;
-		String state;
-		public String getCity() {
-			return city;
-		}
-		public void setCity(String city) {
-			this.city = city;
-		}
-		public String getState() {
-			return state;
-		}
-		public void setState(String state) {
-			this.state = state;
-		}
-		
-		public String toString() {
-			return "<p>City: " + city + ", State: " + state;
-		}	
-	}
-
-	
 	public String toString() {
 		StringBuilder strBldr = new StringBuilder();
-		strBldr.append("<p>Zipcode Information:<p>zip: " + zip_code +
-                ", Country: " + country + ", Country Abbr: " + country_abbr);
-//		strBldr.append(timezone);
-//		strBldr.append("<p>Acceptable City Names:");
-//		if((this.acceptable_city_names != null)) {
-//			for(CityState cityState:acceptable_city_names){
-//				strBldr.append("<p>" + cityState.getCity() + ", " + cityState.getState());
-//			}
-//		}
+		strBldr.append("<p>Zipcode Information:<p>zip: " + post_code +
+				", Country: " + country + ", Country Abbr: " + country_abbr);
+
 		return strBldr.toString();
 	}
+}
+
+class Place {
+	@SerializedName("place name") String place_name;
+	@SerializedName("state abbreviation") String country_abbr;
+	String longitude;
+	String latitude;
+	String state;
+
+	public String getPlace_name() {
+		return place_name;
+	}
+	public void setPlace_name(String place_name) {
+		this.place_name = place_name;
+	}
+	public String getCountry_abbr() {
+		return country_abbr;
+	}
+	public void setCountry_abbr(String country_abbr) {
+		this.country_abbr = country_abbr;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String toString() {
+		StringBuilder strBldr = new StringBuilder();
+		strBldr.append("<p>Place:<p>zip: " + place_name +
+				", State: " + country_abbr );
+
+		return strBldr.toString();
+	}
+}
+
+
+
+
+
 }
 
